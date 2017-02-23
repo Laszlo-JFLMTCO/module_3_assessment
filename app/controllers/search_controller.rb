e63bea122f6151618d3247d8ff78e95de3ac304e
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 10:12:35 -0700

    Fix adding total stores found to the index view page. Rest of user story is still WIP.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
-    response = Faraday.get "https://api.bestbuy.com/v1/stores((area(#{params["searchbox"]},25)))?apiKey=#{ENV['SEARCH_API_KEY']}&format=json"
+    response_raw = Faraday.get "https://api.bestbuy.com/v1/stores((area(#{params["searchbox"]},25)))?apiKey=#{ENV['SEARCH_API_KEY']}&format=json"
+    response = JSON.parse(response_raw.body)
+    @total_stores = response["total"]
+    # @stores = response["stores"].map do |store_raw|
+    #   Store.new(store_raw)
+    # end
+    # binding.pry
 # response = get "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=ENV['SEARCH_API_KEY']&format=json"
 puts "hello"
   end
