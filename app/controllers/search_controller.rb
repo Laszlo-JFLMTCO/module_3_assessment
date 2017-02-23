59ba809cdbb4aed81529213e75484f4ea904dfb6
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 10:02:11 -0700

    Add VCR cassette for search api test. In search_controller build ugly api call dynamically from params and secret key, confirm through pry session that response is coming back.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
-
+    binding.pry
+    response = Faraday.get "https://api.bestbuy.com/v1/stores((area(#{params["searchbox"]},25)))?apiKey=#{ENV['SEARCH_API_KEY']}&format=json"
+# response = get "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=ENV['SEARCH_API_KEY']&format=json"
+puts "hello"
   end
 end
