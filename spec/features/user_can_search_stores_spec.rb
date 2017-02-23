66d1cb9d324b283ae9ceddbd1eb92c3542554fef
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 09:30:55 -0700

    Work with postman and bestbuy API query builder to make sure have a working API call. Correct typo in spec file name. Add code into spec file for api search user story

--- spec/features/user_can_search_stores_spec.rb
+++ spec/features/user_can_search_stores_spec.rb
@@ -2,14 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'User search stores' do
   scenario 'When enters zip code into search box' do
-    
     # As a user
     # When I visit "/"
+    visit root_path
+    
     # And I fill in a search box with "80202" and click "search"
+    within(.navbar) do
+      fill_in :searchbox, with: '80202'
+      click_on 'search'
+    end
+    
     # Then my current path should be "/search" (ignoring params)
+    expect(current_path).to eq(search_path)
+
     # And I should see stores within 25 miles of 80202
     # And I should see a message that says "16 Total Stores"
     # And I should see exactly 10 results
     # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+    expect(page).to have_content('16 Total Stores')
+    expect(page).to have_css(tr, count: 11)
+    expect(page).to have_content('Long name')
+    expect(page).to have_content('City')
+    expect(page).to have_content('Distance')
+    expect(page).to have_content('Phone number')
+    expect(page).to have_content('Store type')
   end
 end
