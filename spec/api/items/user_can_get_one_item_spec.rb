5dc2d90f6a3214a330328ce08673fb011788e860
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 11:42:29 -0700

    Pass test for api endpoint user requesting one specific item details

--- spec/api/items/user_can_get_one_item_spec.rb
+++ spec/api/items/user_can_get_one_item_spec.rb
@@ -4,21 +4,18 @@ RSpec.describe '/api/v1/items' do
   it 'get items' do
     Item.create(name: 'sdfsdf', description: 'sdfsdf', image_url: 'sdfsdfsd')
     # When I send a GET request to `/api/v1/items/1`
-    # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-    # When I send a GET request to `/api/v1/items`
     get "/api/v1/items/1"
 
-    items = JSON.parse(response.body)
+    item = JSON.parse(response.body)
 
-    # I receive a 200 JSON response containing all items
+    # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
     expect(response).to have_http_status(200)
-    # And each item has an id, name, description, and image_url but not the created_at or updated_at
-    expect(items.first).to have_key("id")
-    expect(items.first).to have_key("name")
-    expect(items.first).to have_key("description")
-    expect(items.first).to have_key("image_url")
-    expect(items.first).not_to have_key("created_at")
-    expect(items.first).not_to have_key("updated_at")
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("image_url")
+    expect(item).not_to have_key("created_at")
+    expect(item).not_to have_key("updated_at")
   end
 
 # When I send a DELETE request to `/api/v1/items/1`
