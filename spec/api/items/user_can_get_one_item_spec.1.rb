4d55484bc8094de4368ead66cbefcf2ca8fe91af
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 11:35:58 -0700

    Add status code to items index api endpoint. Create drat of specific item endpint user story. Update items serializer to eliminate returning updated_at and created_at

--- spec/api/items/user_can_get_one_item_spec.1.rb
+++ spec/api/items/user_can_get_one_item_spec.1.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe '/api/v1/items' do
   it 'get items' do
     Item.create(name: 'sdfsdf', description: 'sdfsdf', image_url: 'sdfsdfsd')
+    # When I send a GET request to `/api/v1/items/1`
+    # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
     # When I send a GET request to `/api/v1/items`
-    get "/api/v1/items"
+    get "/api/v1/items/1"
 
     items = JSON.parse(response.body)
 
@@ -18,4 +20,11 @@ RSpec.describe '/api/v1/items' do
     expect(items.first).not_to have_key("created_at")
     expect(items.first).not_to have_key("updated_at")
   end
+
+# When I send a DELETE request to `/api/v1/items/1`
+# I receive a 204 JSON response if the record is successfully deleted
+
+# When I send a POST request to `/api/v1/items` with a name, description, and image_url
+# I receive a 201 JSON  response if the record is successfully created
+# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
 end
