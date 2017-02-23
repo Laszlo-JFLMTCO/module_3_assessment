169d3da72e81fb386e402f94d521f0786507f775
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 11:19:36 -0700

    Feeling super stupid, to miss adding items into the test database... Thank you Casey for the eye opener

--- spec/api/items/user_can_get_items_spec.rb
+++ spec/api/items/user_can_get_items_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe '/api/v1/items' do
-  scenario 'get items' do
+  it 'get items' do
+    Item.create(name: 'sdfsdf', description: 'sdfsdf', image_url: 'sdfsdfsd')
     # When I send a GET request to `/api/v1/items`
     get "/api/v1/items"
 
-    json = JSON.parse(response.body)
+    items = JSON.parse(response.body)
 
     binding.pry
 
     # I receive a 200 JSON response containing all items
-    expect(response.first).to have_http_status(200)
+    expect(response).to have_http_status(200)
     # And each item has an id, name, description, and image_url but not the created_at or updated_at
-    expect(response.first).to have_key(:id)
-    expect(response.first).to have_key("id")
-    expect(response.first).to have_key("name")
-    expect(response.first).to have_key("description")
-    expect(response.first).to have_key("image_url")
-    expect(response.first).not_to have_key("created_at")
-    expect(response.first).not_to have_key("updated_at")
+    expect(items.first).to have_key("id")
+    expect(items.first).to have_key("name")
+    expect(items.first).to have_key("description")
+    expect(items.first).to have_key("image_url")
+    expect(items.first).not_to have_key("created_at")
+    expect(items.first).not_to have_key("updated_at")
   end
 
 # When I send a GET request to `/api/v1/items/1`
