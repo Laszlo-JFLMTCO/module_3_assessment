0dd4b4004299b400a5a820ee3b12d960a52bcaa5
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 12:00:46 -0700

    Create route and controller code for item create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,6 +7,16 @@ class Api::V1::ItemsController < ApplicationController
     render :json => Item.find(params[:id]), :status => 200
   end
 
+  def create
+    new_item = Item.new_item
+    new_item.name = 
+    new_item.description = 
+    new_item.image_url =
+    if new_item.save then
+      render :json => Item.last, :status => 201
+    end
+  end
+
   def destroy
     render :json => {}, :status => 204 if Item.delete(params[:id])>0
   end
