a453bfee4bc3c92ae7b206856a093c0b5c69fc02
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-23 09:45:35 -0700

    Update search for with get search. Add /search route to routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  get '/search' => 'searchcontroller#index'
+  
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
